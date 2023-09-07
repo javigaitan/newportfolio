4016e321adf988357c30d0f82115f5f0a27722d7
Gaitan Javier Emanuel <javii.gaitan@gmail.com>
2023-09-07 10:28:06 -0300

    add(portfolio):se suma qa manual

--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -13,6 +13,8 @@ import Hidromiel from '../../assets/Proyectos/Freyr Hidromiel.png';
 import Portafolio from '../../assets/Proyectos/Portafolio.png';
 import Footer from '../../component/Footer/Footer';
 import One from '../../assets/Proyectos/Somos One Touch.png';
+import QA from '../../assets/Proyectos/Mercury Tours.png';
+
 
 
 
@@ -25,6 +27,13 @@ const Portfolio = () => {
     <>
         <Navbar/>
         <div className="bg-gray-100 ">
+        <div className="flex flex-col justify-center items-center">
+                        <div className="md:text-5xl text-4xl font-black text-center text-gray-800 dark:text-white leading-snug lg:w-3/4">
+                            <h2>Deployed projects</h2>
+                        </div>
+                        </div>
+            
+        
                 {/* Remove py-8 */}
                 <div className="mx-auto container py-8">
                     <div className="flex flex-wrap items-center lg:justify-between justify-center">
@@ -375,6 +384,79 @@ const Portfolio = () => {
                     
                 </div>
             </div>
+
+            <div className="flex flex-col justify-center items-center">
+                        <div className="md:text-5xl text-4xl font-black text-center text-gray-800 dark:text-white leading-snug lg:w-3/4">
+                            <h2>Testing QA </h2>
+                        </div>
+                        </div>
+
+                        <div className="mx-auto container py-8">
+                    <div className="flex flex-wrap items-center lg:justify-between justify-center">
+                        {/* Card 1 */}
+                        <div className="mx-2 w-72 lg:mb-0 mb-8">
+                            <div>
+                                <img src={QA} className="w-full h-44" />
+                            </div>
+                            <div className="bg-white">
+                                <div className="flex items-center justify-between px-4 pt-4">
+                                    
+                                </div>
+                                <div className="p-4">
+                                    <div className="flex items-center">
+                                        <h2 className="text-lg font-semibold">Mercury Tours</h2>
+                                    </div>
+                                    <p className="text-xs text-gray-600 mt-2">Se realizan diversas pruebas  de desempeño en el sitio (URL: https://demo.guru99.com/test/newtours/index.php) para validar bugs, criticidad, y generar informes efectivos para mejorar el desempeño.</p>
+                                    
+                                    <div className="flex items-center justify-between py-4">
+                                        <Link to="https://drive.google.com/drive/folders/1eKSUfybA8kiNsafNXVIWYo_wiByLO1kT?usp=sharing" className="text-indigo-700 text-xl font-semibold hover:text-indigo-900">
+                                            Informe
+                                            </Link>
+                                            </div>
+                                    </div>
+                               </div>
+                            </div>
+                        {/* Card 1 Ends */}
+                        {/* Card 1 
+                        <div className="mx-2 w-72 lg:mb-0 mb-8">
+                            <div>
+                                <img src={Nova} className="w-full h-44" />
+                            </div>
+                            <div className="bg-white">
+                                <div className="flex items-center justify-between px-4 pt-4">
+                                    
+                                </div>
+                                <div className="p-4">
+                                    <div className="flex items-center">
+                                        <h2 className="text-lg font-semibold">Nova Academy</h2>
+                                    </div>
+                                    <p className="text-xs text-gray-600 mt-2">Un servicio para compra y venta de cursos, con la posibilidad como usuario de comprar y vender contenido educativo. Conectado con Mercado Pago y con un administrador para gestionar cursos y usuario existentes.</p>
+                                    <div className="flex mt-4">
+                                        <div>
+                                            <p className="text-xs text-gray-600 px-2 bg-gray-200 py-1">React</p>
+                                        </div>
+                                        <div className="pl-2">
+                                            <p className="text-xs text-gray-600 px-2 bg-gray-200 py-1">Js</p>
+                                        </div>
+                                        <div className="pl-2">
+                                            <p className="text-xs text-gray-600 px-2 bg-gray-200 py-1">Tailwind CSS</p>
+                                        </div>
+                                        <div className="pl-2">
+                                            <p className="text-xs text-gray-600 px-2 bg-gray-200 py-1">NodeJs</p>
+                                        </div>
+                                    </div>
+                                    <div className="flex items-center justify-between py-4">
+                                        <Link to="https://pf-nova-academy.vercel.app/#/" className="text-indigo-700 text-xl font-semibold hover:text-indigo-900">
+                                            Informe
+                                            </Link>
+                                            </div>
+                                    </div>
+                               </div>
+                            </div>
+                        {/* Card 1 Ends */}
+                        </div>
+                        </div>
+
 
             <Footer/>
             </>
